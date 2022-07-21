259a043bec3d3a61c0aa4dc94fcc9f2d07c375b1
Safal Paliwal <safalpaliwal@gmail.com>
2022-07-21 20:11:08 +0530

    intial app

---
+++ main.js
@@ -0,0 +1,59 @@
+const texts = document.querySelector(".texts");
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement("p");
+
+recognition.addEventListener("result", (e) => {
+  texts.appendChild(p);
+  const text = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join("");
+
+  p.innerText = text;
+  if (e.results[0].isFinal) {
+    if (text.includes("how are you")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "I am Excellent!! What about you ?";
+      texts.appendChild(p);
+    }
+    if (
+      text.includes("what's your name") ||
+      text.includes("what is your name")
+    ) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "My Name is Christopher";
+      texts.appendChild(p);
+    }
+    if (text.includes("open YouTube")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "opening youtube channel";
+      texts.appendChild(p);
+      console.log("opening youtube");
+      window.open("https://www.youtube.com/");
+    }
+    if (text.includes("open my app")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "opening vector share";
+      texts.appendChild(p);
+      console.log("opening vector-share");
+      window.open("https://vector-share.herokuapp.com/");
+    }
+    p = document.createElement("p");
+  }
+});
+
+recognition.addEventListener("end", () => {
+  recognition.start();
+});
+
+recognition.start();
